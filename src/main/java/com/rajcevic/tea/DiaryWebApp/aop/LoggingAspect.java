66401d94f6958fca17f8811216a64a2e6694c407
TeaRajcevic <tea.rajcevich@gmail.com>
2020-01-31 19:34:10 +0100

    AOP - Logging

---
+++ src/main/java/com/rajcevic/tea/DiaryWebApp/aop/LoggingAspect.java
@@ -0,0 +1,49 @@
+package com.rajcevic.tea.DiaryWebApp.aop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.rajcevic.tea.DiaryWebApp.web..*(..)))")
+    public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
+    {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        //Get intercepted method details
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        final StopWatch stopWatch = new StopWatch();
+
+        //Measure method execution time
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        //Log method execution time
+        LOGGER.info("Execution time of " + className + "." + methodName + " :: " + stopWatch.getTotalTimeMillis() + " ms");
+
+        return result;
+    }
+
+    @Around("execution(void com.rajcevic.tea.DiaryWebApp.data.UserRepositoryImpl.saveUserWithAuthorities(..)))")
+    public Object savedUser(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
+    {
+        Object result = proceedingJoinPoint.proceed();
+
+        //Log method execution time
+        LOGGER.info("New user registered!");
+
+        return result;
+    }
+}
