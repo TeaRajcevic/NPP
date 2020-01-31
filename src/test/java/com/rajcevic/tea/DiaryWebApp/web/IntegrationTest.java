b99f744e62d54120fa470d0f8509a9e5b0008d21
TeaRajcevic <tea.rajcevich@gmail.com>
2020-01-31 19:12:44 +0100

    Integration and JUnit tests

---
+++ src/test/java/com/rajcevic/tea/DiaryWebApp/web/IntegrationTest.java
@@ -0,0 +1,141 @@
+package com.rajcevic.tea.DiaryWebApp.web;
+
+import com.rajcevic.tea.DiaryWebApp.data.ImageRepository;
+import com.rajcevic.tea.DiaryWebApp.data.UserRepository;
+import com.rajcevic.tea.DiaryWebApp.model.Image;
+import com.rajcevic.tea.DiaryWebApp.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class IntegrationTest {
+
+    private Image testImage1;
+    private Image testImage2;
+    private Image testImage3;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    public void prepareImagesForTest() {
+        testImage1 = new Image();
+        testImage1.setTitle("Castle");
+        testImage1.setImagedata("ImageData");
+        testImage1.setFormat("JPG");
+        testImage1.setDescription("Description");
+        testImage1.setTags("architecture");
+        testImage1.setUploader("Admin");
+        testImage1.setSizex("600");
+        testImage1.setSizey("400");
+
+        entityManager.persist(testImage1);
+
+        testImage2 = new Image();
+        testImage2.setTitle("Palace");
+        testImage2.setImagedata("ImageData");
+        testImage2.setFormat("PNG");
+        testImage2.setDescription("Description");
+        testImage2.setTags("architecture");
+        testImage2.setUploader("Random User");
+        testImage2.setSizex("2560");
+        testImage2.setSizey("1440");
+
+        entityManager.persist(testImage2);
+
+        testImage3 = new Image();
+        testImage3.setTitle("Tree");
+        testImage3.setImagedata("ImageData");
+        testImage3.setFormat("PNG");
+        testImage3.setDescription("Description");
+        testImage3.setTags("nature");
+        testImage3.setUploader("Random User");
+        testImage3.setSizex("640");
+        testImage3.setSizey("480");
+
+        entityManager.persist(testImage3);
+
+        entityManager.flush();
+    }
+
+    @Test
+    public void whenFindByName_thenReturnUser() {
+        // given
+        User test = new User("ime", "prezime", "free");
+        entityManager.persist(test);
+        entityManager.flush();
+
+        // when
+        User found = userRepository.findByUsername(test.getUsername());
+
+        // then
+        assertThat(found.getUsername())
+                .isEqualTo(test.getUsername());
+    }
+
+    @Test
+    public void whenFindByTitle_thenReturnImage() {
+        // given
+        prepareImagesForTest();
+
+        // when
+        List<Image> found = imageRepository.findByTags("architecture");
+
+        // then
+        assertThat(found.size()).isEqualTo(2);
+        assertThat(found.get(0).getTitle())
+                .isEqualTo("Castle");
+
+        assertThat(found.get(1).getTitle())
+                .isEqualTo("Palace");
+
+    }
+
+    @Test
+    public void whenFindByTags_thenReturnImage() {
+        // given
+        prepareImagesForTest();
+
+        // when
+        List<Image> found = imageRepository.findByTitle("Castle");
+
+        // then
+        assertThat(found.size()).isEqualTo(1);
+        assertThat(found.get(0).getTags())
+                .isEqualTo("architecture");
+
+
+    }
+
+    @Test
+    public void whenFindByUploader_thenReturnImage() {
+        // given
+        prepareImagesForTest();
+        User test = new User("Random User", "prezime", "free");
+        entityManager.persist(test);
+        entityManager.flush();
+
+        // when
+        List<Image> found = imageRepository.findByUploader("Random User");
+
+        // then
+        assertThat(found.size()).isEqualTo(2);
+        assertThat(found.get(0).getUploader())
+                .isEqualTo("Random User");
+    }
+}
---
+++ src/test/java/com/rajcevic/tea/DiaryWebApp/web/LoginControllerTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rajcevic.tea.DiaryWebApp.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testFormLoginAdmin() throws Exception {
+        this.mockMvc
+                .perform(formLogin("/login").user("username", "admin").password("password", "adminpass"))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+    }
+
+    @Test
+    public void testFormLoginWrong() throws Exception {
+        this.mockMvc
+                .perform(formLogin("/login").user("username", "bzvz").password("password", "bzvz"))
+                .andExpect(unauthenticated());
+    }
+}
