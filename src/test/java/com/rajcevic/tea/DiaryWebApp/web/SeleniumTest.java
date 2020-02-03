96371fb792623bd5b4a9f30fa12ea820f242217f
TeaRajcevic <tea.rajcevich@gmail.com>
2020-02-03 20:23:08 +0100

    Added asserts to Selenium tests

--- src/test/java/com/rajcevic/tea/DiaryWebApp/web/SeleniumTest.java
+++ src/test/java/com/rajcevic/tea/DiaryWebApp/web/SeleniumTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
 
 
